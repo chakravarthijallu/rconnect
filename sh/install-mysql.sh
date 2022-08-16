38bbae29ef4d5754dd1196d0f947a22f7f8a8592
chakravarthi <chakrijallu7@gmail.com>
2022-08-16 15:45:00 +0530

    second commit

--- sh/install-mysql.sh
+++ sh/install-mysql.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-function checkMysql() {
-    sudo dpkg -s mysql-server-8.0 2>> /dev/null >> /dev/null
-    local MYSQLSERVERINSTALL=$?
-    if [ $MYSQLSERVERINSTALL -ne 0 ]
-    then
-       echo "MYSQL SERVER IS NOT INSTALLED PLEASE INSTALL IT"
-        
-    else
-       echo "MYSQL SERVER IS INSTALLED PROPERLY "
-    fi
+function checkMysql () {
+     sudo dpkg -s mysql-server-8.0 2>> /dev/null >> /dev/null
+     local CHECKMYSQLSTATUS=$?
+     if [ $CHECKMYSQLSTATUS -ne 0 ]
+     then
+         echo "PLEASE INSTALL MYSQL SERVER"
+         installMysql
+     else
+         echo "MYSQL IS ALREADY INSTALLED"
+     fi
 }
 
-function installMysql() {
-    sudo apt install -y mysql-server-8.0 2>> /dev/null >> /dev/null
-} 
+function installMysql () {
+     sudo apt install -y mysql-server-8.0 2>> /dev/null >> /dev/null
+     local MYSQLINSTALLSTATUS=$?
+     if [ $MYSQLINSTALLSTATUS -ne 0 ]
+     then
+         echo "MYSQL DB IS NOT INSTALLED PROPERLY"
+         exit
+     else
+         echo "MYSQL SERVER INSTALLED SUCCESSFULLY"
+     fi
+}
+
+# Main 
+checkMysql
